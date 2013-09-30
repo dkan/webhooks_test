516b612f62e78ef515532fe70fecd9d5e8c60d38
Damien <damien.kan@gmail.com>
2013-09-30 12:04:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebhooksApp::Application.config.secret_token = 'e9758884e4b81a230ccde042d81f7cc2beb2b5fee8280f1f517c9b3aec7854a12497f30f2bc5225d6a97dfb0a448c7ecd3ef96da24c5f74c3159fe45b59e4172'
